507acb1aa702cca0257f266c3980f48a025125d2
Joel Miller <jmiller4@oberlin.edu>
2018-03-19 17:00:29 -0400

    Add alphabetical name selection
    Added the functionality to select names in order from an alphabetized list.
    Also added some more comments and changed the syntax of the tweets.

--- Bot.py
+++ Bot.py
@@ -4,26 +4,58 @@ from Secrets import *
 import io
 
 # This is a twitter bot whose purpose is to tweet messages about which starbucks drinks people are drinking.
-# Everybody loves starbucks!
+# Everybody loves starbucks.
 
 # AUTHOR (code): Joel Miller
 # CONCEPT: Jesi Gaston
 
+#this variable represents wether we go through names alphabetically or not (if not, we choose randomly).
+alphabetically = True
+
+# One execution of main() posts one tweet.
 def main():
 
-    # Twitter API stuff
+    # Log in to twitter
     auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
     auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
     api = tweepy.API(auth)
 
-    # Making the tweet
+    # Make the tweet
     random.seed()
-
-    name = pick_random_name()
+    name = choose_baby_name(alphabetically)
     drink = pick_random_line("Menu.txt", False)
     tweet = construct_tweet(name, drink)
 
+    # Post the tweet
     api.update_status(tweet)
+
+# this function chooses a baby name, either moving alphabetically down a list of names or choosing one randomly.
+# in_order should be a boolean that represents whether or not the names are being chose in order.
+def choose_baby_name(in_order):
+
+    if in_order:
+        # first, we need to figure out where in the list of names we are. To help with that, the bot maintains a file
+        # called log.txt which just holds the current line number. It will start at 0.
+        log_file = open("log.txt", 'r+')
+        line_number = int(log_file.readline().split()[0])
+
+        # retrieve the appropriate name from the alphabetical list of names
+        name_file = open("Names3.txt")
+        name_lines = name_file.readlines()
+        name = name_lines[line_number].split()[0]
+        name_file.close()
+
+        # write the next line number to the log file
+        next_line = (line_number + 1) % (len(name_lines) - 1)
+        log_file.seek(0)
+        log_file.truncate()
+        log_file.write(str(next_line))
+        log_file.close()
+
+    else:
+        name = pick_random_name()
+
+    return name
 
 
 # This function takes in a baby name and a drink name, and constructs the actual tweet text.
@@ -31,35 +63,39 @@ def main():
 def construct_tweet(name, drink):
 
     tweet = name
-    tweet += " is having a"
+    tweet += " loves the "
 
-    if drink[0] in ['a','e','i','o','u','A','E','I','O','U']:
-        tweet += "n "
-    else:
-        tweet += " "
+    # This was from when the tweets had different syntax. I'm keeping this in just in case we decide to go back.
+    #
+    # # Here, we make sure that our sentence is grammatically correct.
+    # if drink[0] in ['a','e','i','o','u','A','E','I','O','U']:
+    #     tweet += "n "
+    # else:
+    #     tweet += " "
 
     tweet += drink + "."
 
     return tweet
 
 
-# This function takes in a line from the text file, and removes all the stuff besides the name.
+# This function takes in a line from Names1 or Names2.txt and removes all the stuff besides the name.
 # It also gives the name the appropriate casing (e.g. ALEX --> Alex)
 def clean_line(line):
 
     capitalized_name = line.split()[0]
-    normal_name = capitalized_name[0]
-    normal_name += capitalized_name[1:len(capitalized_name)].lower()
+    normal_name = capitalized_name[0] + capitalized_name[1:].lower()
     return normal_name
 
 
-# picks a random line out of a text file. It takes in the name/ path of the file, as well as the number of lines in the file.
+# This function picks a random line out of a text file. It takes in the name/ path of the file,
+# and an optional parameter to ignore the last line of the file.
 def pick_random_line(file_name, include_last_line = True):
 
     file = io.open(file_name, mode="r", encoding="utf-8")
     line_list = file.readlines()
 
-    # for drink names, the last line is a newline, so we don't want to include it.
+    # for drink names, the last line is blank, so we don't want to include it
+    # (include_last_line will be passed in as false).
     if not include_last_line:
         line_list = line_list[0:len(line_list) - 1]
 
@@ -69,8 +105,8 @@ def pick_random_line(file_name, include_last_line = True):
 
     # here we take the last character off of the line.
     # this isn't so important for baby names, because those lines will be cleaned more later.
-    # but it is important for drink names, which have a newline at the end, and don't get cleaned
-    # after being returned from this function.
+    # but it is important for drink names, which have a newline character at the end,
+    # and don't get cleaned after being returned from this function.
     line = line[0:len(line)-1]
 
     return line
@@ -84,6 +120,4 @@ def pick_random_name():
     raw_line = pick_random_line(file_name)
     return clean_line(raw_line)
 
-
 main()
-
